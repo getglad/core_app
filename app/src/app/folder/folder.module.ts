fb44f2e28c93c88c08f88331a9ad9b5810a391bb
getglad <1sembyl8p@mozmail.com>
2023-02-26 09:19:51 +0000

    add supabase auth

--- app/src/app/folder/folder.module.ts
+++ app/src/app/folder/folder.module.ts
@@ -1,20 +1,23 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { FolderPageRoutingModule } from './folder-routing.module';
 
+import { AccountComponent } from '../account/account.component';
+import { AuthComponent } from '../auth/auth.component';
 import { FolderPage } from './folder.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
-    FolderPageRoutingModule
+    FolderPageRoutingModule,
   ],
-  declarations: [FolderPage]
+  declarations: [FolderPage, AccountComponent, AuthComponent],
 })
 export class FolderPageModule {}
