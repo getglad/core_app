06f2568be4abd558280759f735afbba2e697df36
getglad <1sembyl8p@mozmail.com>
2023-05-09 04:23:27 +0000

    nav bar + mobile

--- app/src/app/components/topnav/topnav.component.ts
+++ app/src/app/components/topnav/topnav.component.ts
@@ -12,7 +12,26 @@ export class TopnavComponent implements OnInit {
   @Input()
   name!: string;
 
+  block: string = 'block';
+  blockOpen: string = 'hidden';
+  collapseOpen: string = '';
+  openPadding: string = '';
+
   constructor() {}
 
   ngOnInit() {}
+
+  openMenu() {
+    if (this.collapseOpen == '') {
+      this.collapseOpen = 'collapse-open';
+      this.openPadding = 'pb-3';
+      this.block = 'hidden';
+      this.blockOpen = 'block';
+    } else {
+      this.collapseOpen = '';
+      this.openPadding = '';
+      this.block = 'block';
+      this.blockOpen = 'hidden';
+    }
+  }
 }
